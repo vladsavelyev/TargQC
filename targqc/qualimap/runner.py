de3dab79b09afa7727b21c4e006c23726786a861
vladsaveliev <vladsaveliev@me.com>
2016-06-29 18:08:17 +0300

    Add project-level summary

--- targqc/qualimap/runner.py
+++ targqc/qualimap/runner.py
@@ -1,8 +1,13 @@
-from os.path import getsize, dirname, join, abspath
+import subprocess
+from distutils.version import LooseVersion
+from os.path import getsize, dirname, join, abspath, relpath, isdir, exists
+from os import listdir
+import shutil
 
 from Utils.call_process import run
-from Utils.file_utils import safe_mkdir, verify_file
-from Utils.logger import info
+from Utils.file_utils import safe_mkdir, verify_file, verify_dir, file_transaction
+from Utils.logger import info, warn
+from Utils.reporting.reporting import write_tsv_rows
 
 import targqc.config as tc
 
@@ -11,6 +16,9 @@ def get_qualimap_max_mem(bam):
     mem_m = getsize(bam) / 3 / 1024 / 1024
     mem_m = min(max(mem_m, 1200), 16000)
     return mem_m
+
+
+qualimap = abspath(join(dirname(__file__), 'qualimap', 'qualimap'))
 
 
 def fix_bed_for_qualimap(bed_fpath, qualimap_bed_fpath):
@@ -49,16 +57,150 @@ def run_qualimap(output_dir, bam_fpath, bed_fpath=None, threads=1):
         bed_cmd = ' -gff ' + qualimap_bed_fpath + ' '
         info('Using amplicons/capture panel ' + qualimap_bed_fpath)
 
-    qualimap = abspath(join(dirname(__file__), 'qualimap', 'qualimap'))
-
     mem_cmdl = ''
     mem_m = get_qualimap_max_mem(bam_fpath)
     mem = str(int(mem_m)) + 'M'
     mem_cmdl = '--java-mem-size=' + mem
 
-    cmdline = ('{qualimap} bamqc --skip-duplicated -nt {threads} {mem_cmdl} -nr 5000 '
+    cmdline = (qualimap + ' bamqc --skip-duplicated -nt {threads} {mem_cmdl} -nr 5000 '
         '-bam {bam_fpath} -outdir {output_dir} {bed_cmd} -c -gd HUMAN').format(**locals())
     report_fpath = join(output_dir, 'qualimapReport.html')
 
     run(cmdline, output_fpath=report_fpath, stdout_to_outputfile=False, env_vars=dict(DISPLAY=None),
         checks=[lambda _1, _2: verify_file(report_fpath)], reuse=tc.reuse_intermediate)
+
+
+def run_multisample_qualimap(output_dir, work_dir, samples, targqc_full_report):
+    """ 1. Generates Qualimap2 plots and put into plots_dirpath
+        2. Adds records to targqc_full_report.plots
+    """
+    plots_dirpath = join(output_dir, 'plots')
+    if tc.reuse_intermediate and isdir(plots_dirpath) and [f for f in listdir(plots_dirpath) if not f.startswith('.')]:
+        info('Qualimap miltisample plots exist - ' + plots_dirpath + ', reusing...')
+    else:
+        # Qualimap2 run for multi-sample plots
+        if len([s.qualimap_html_fpath for s in samples if s.qualimap_html_fpath]) > 0:
+            if qualimap is not None and get_qualimap_type(qualimap) == 'full':
+                qualimap_output_dir = join(work_dir, 'qualimap_multi_bamqc')
+
+                _correct_qualimap_genome_results(samples)
+                _correct_qualimap_insert_size_histogram(samples)
+
+                safe_mkdir(qualimap_output_dir)
+                rows = []
+                for sample in samples:
+                    if sample.qualimap_html_fpath:
+                        rows += [[sample.name, sample.qualimap_html_fpath]]
+
+                data_fpath = write_tsv_rows(([], rows), join(qualimap_output_dir, 'qualimap_results_by_sample.tsv'))
+                qualimap_plots_dirpath = join(qualimap_output_dir, 'images_multisampleBamQcReport')
+                cmdline = qualimap + ' multi-bamqc --data {data_fpath} -outdir {qualimap_output_dir}'.format(**locals())
+                run(cmdline, env_vars=dict(DISPLAY=None),
+                    checks=[lambda _1, _2: verify_dir(qualimap_output_dir)], reuse=tc.reuse_intermediate)
+
+                if not verify_dir(qualimap_plots_dirpath):
+                    warn('Warning: Qualimap for multi-sample analysis failed to finish. TargQC will not contain plots.')
+                    return None
+                else:
+                    if exists(plots_dirpath):
+                        shutil.rmtree(plots_dirpath)
+                    shutil.move(qualimap_plots_dirpath, plots_dirpath)
+            else:
+                warn('Warning: Qualimap for multi-sample analysis was not found. TargQC will not contain plots.')
+                return None
+
+    targqc_full_report.plots = []
+    for plot_fpath in listdir(plots_dirpath):
+        plot_fpath = join(plots_dirpath, plot_fpath)
+        if verify_file(plot_fpath) and plot_fpath.endswith('.png'):
+            targqc_full_report.plots.append(relpath(plot_fpath, output_dir))
+
+
+def get_qualimap_type(tool_cmdline):
+    """Qualimap supports multi-bamqc functionality only starting from v.2.0
+    """
+    if LooseVersion(_get_qualimap_version(tool_cmdline)) >= LooseVersion("2.0"):
+        return "full"
+    else:
+        return "limited"
+
+
+def _get_qualimap_version(tool_cmdline):
+    cmdline = tool_cmdline + ' -version'  # actually, Qualimap doesn't have -version option
+
+    version = None
+    with subprocess.Popen(cmdline,
+                          stdout=subprocess.PIPE,
+                          stderr=subprocess.STDOUT,
+                          shell=True).stdout as stdout:
+        out = stdout.read().strip()
+        flag = "QualiMap v."
+        if out.startswith(flag) >= 0:
+            version = out.split(flag)[-1].strip()
+    if not version:
+        info('WARNING: could not determine Qualimap version, using 1.0')
+        return '1.0'
+    if version.split('.') > 2:  # only major version
+        version = '.'.join(version.split('.')[:2])
+    return version
+
+
+def _correct_qualimap_genome_results(samples):
+    """ fixing java.lang.Double.parseDouble error on entries like "6,082.49"
+    """
+    for s in samples:
+        if verify_file(s.qualimap_genome_results_fpath):
+            correction_is_needed = False
+            with open(s.qualimap_genome_results_fpath, 'r') as f:
+                content = f.readlines()
+                metrics_started = False
+                for line in content:
+                    if ">> Reference" in line:
+                        metrics_started = True
+                    if metrics_started:
+                        if line.find(',') != -1:
+                            correction_is_needed = True
+                            break
+            if correction_is_needed:
+                with open(s.qualimap_genome_results_fpath, 'w') as f:
+                    metrics_started = False
+                    for line in content:
+                        if ">> Reference" in line:
+                            metrics_started = True
+                        if metrics_started:
+                            if line.find(',') != -1:
+                                line = line.replace(',', '')
+                        f.write(line)
+
+
+def _correct_qualimap_insert_size_histogram(samples):
+    """ replacing Qualimap insert size histogram with Picard one.
+    """
+    for s in samples:
+        qualimap1_dirname = dirname(s.qualimap_ins_size_hist_fpath).replace('raw_data_qualimapReport', 'raw_data')
+        qualimap2_dirname = dirname(s.qualimap_ins_size_hist_fpath)
+        if exists(qualimap1_dirname):
+            if not exists(qualimap2_dirname):
+                shutil.move(qualimap1_dirname, qualimap2_dirname)
+            else:
+                shutil.rmtree(qualimap1_dirname)
+        elif not exists(qualimap2_dirname):
+            continue  # no data from both Qualimap v.1 and Qualimap v.2
+
+        # if qualimap histogram exits and reuse_intermediate, skip
+        if verify_file(s.qualimap_ins_size_hist_fpath, silent=True) and tc.reuse_intermediate:
+            pass
+        else:
+            if verify_file(s.picard_ins_size_hist_txt_fpath):
+                with open(s.picard_ins_size_hist_txt_fpath, 'r') as picard_f:
+                    one_line_to_stop = False
+                    for line in picard_f:
+                        if one_line_to_stop:
+                            break
+                        if line.startswith('## HISTOGRAM'):
+                            one_line_to_stop = True
+
+                    with file_transaction(work_dir, s.qualimap_ins_size_hist_fpath) as tx:
+                        with open(tx, 'w') as qualimap_f:
+                            for line in picard_f:
+                                qualimap_f.write(line)
---
+++ targqc/summarize.py
@@ -0,0 +1,390 @@
+import shutil
+import os
+from os import listdir
+from os.path import relpath, join, exists, dirname, basename, abspath, splitext
+from collections import OrderedDict, defaultdict
+
+import targqc
+import targqc.config as tc
+from Utils.bam_bed_utils import verify_bed
+from Utils.file_utils import verify_dir, verify_file, adjust_path, symlink_plus, file_transaction, add_suffix
+from Utils.logger import info, err, debug
+from Utils.reporting.reporting import PerRegionSampleReport, BaseReport, Metric, ReportSection, MetricStorage, \
+    FullReport
+from targqc.general_report import get_header_metric_storage
+from targqc.prep_bed import prepare_beds
+from targqc.qualimap.runner import run_multisample_qualimap
+from targqc.region_coverage import get_detailed_metric_storage
+
+
+def _make_targetcov_symlinks(samples):
+    for sample in samples:
+        new_link = join(
+            dirname(dirname(sample.targetcov_detailed_txt)),
+            basename(sample.targetcov_detailed_txt))
+        if exists(new_link):
+            os.unlink(new_link)
+        symlink_plus(sample.targetcov_detailed_txt, new_link)
+        info('TargetCov TXT symlink saved to ' + new_link)
+
+
+def _make_tarqc_html_report(output_dir, work_dir, samples, bed_fpath=None, tag_by_sample=None):
+    header_storage = get_header_metric_storage(tc.depth_thresholds,
+                                               is_wgs=bed_fpath is not None,
+                                               padding=tc.padding)
+
+    jsons_by_sample = {s.name: s.targqc_json_fpath for s in samples if verify_file(s.targqc_json_fpath)}
+    htmls_by_sample = {s.name: s.targqc_html_fpath for s in samples if verify_file(s.targqc_html_fpath)}
+
+    if not jsons_by_sample or not htmls_by_sample:
+        return None, None, None
+
+    targqc_full_report = FullReport.construct_from_sample_report_jsons(samples, output_dir, jsons_by_sample, htmls_by_sample)
+
+    for sample_report in targqc_full_report.sample_reports:
+        if tag_by_sample:
+            sample_report.set_project_tag(tag_by_sample[sample_report.sample.name])
+        if verify_file(sample_report.sample.qualimap_html_fpath):
+            url = relpath(sample_report.sample.qualimap_html_fpath, output_dir)
+            r = sample_report.find_record(sample_report.records, 'Qualimap')
+            if r:
+                r.url = url
+            else:
+                sample_report.add_record(metric_name='Qualimap', value='Qualimap', url=url, silent=True)
+
+    run_multisample_qualimap(output_dir, work_dir, samples, targqc_full_report)
+
+    fn = splitext(basename(samples[0].targqc_txt_fpath))[0]
+    txt_fpath = targqc_full_report.save_txt(join(output_dir, fn + '.txt'))
+    tsv_fpath = targqc_full_report.save_tsv(join(output_dir, fn + '.tsv'))
+    html_fpath = targqc_full_report.save_html(join(output_dir, fn + '.html'), 'TargQC')
+
+    return txt_fpath, tsv_fpath, html_fpath
+
+
+def _combined_regional_reports(work_dir, output_dir, samples):
+    if not any(verify_file(s.targqc_region_tsv, silent=True) for s in samples):
+        return None, None
+
+    txt_region_rep_fpath = join(output_dir, basename(samples[0].targqc_region_txt))
+    tsv_region_rep_fpath = join(output_dir, basename(samples[0].targqc_region_tsv))
+    debug('Combining regional reports, writing to ' + txt_region_rep_fpath + ' and ' + tsv_region_rep_fpath)
+    with file_transaction(work_dir, tsv_region_rep_fpath) as tx_tsv, file_transaction(work_dir, txt_region_rep_fpath) as tx_txt:
+        with open(tx_tsv, 'w') as tsv_out, open(tx_txt, 'w') as txt_out:
+            for sample_i, s in enumerate(samples):
+                with open(s.targqc_region_txt) as txt_in:
+                    for l in txt_in:
+                        if l.startswith('#'):
+                            if not l.startswith('##') and sample_i == 0:
+                                txt_out.write('#Sample' + ' '*(max(len('#Sample'), len(s.name)) - len('#Sample')) + ' ' + l.replace('#Chr', 'Chr '))
+                        else:
+                            txt_out.write(s.name + ' '*(max(len('#Sample'), len(s.name)) - len(s.name)) + ' ' + l)
+                with open(s.targqc_region_tsv) as tsv_in:
+                    for l in tsv_in:
+                        if l.startswith('#'):
+                            if not l.startswith('##') and sample_i == 0:
+                                tsv_out.write('#Sample\t' + l[1:])
+                        else:
+                            tsv_out.write(s.name + '\t' + l)
+
+    return txt_region_rep_fpath, tsv_region_rep_fpath
+
+
+def summarize_targqc(summary_threads, output_dir, work_dir, samples, bed_fpath=None, features_fpath=None, tag_by_sample=None):
+    info('TargQC coverage statistics for all samples')
+
+    txt_fpath, tsv_fpath, html_fpath = _make_tarqc_html_report(output_dir, work_dir, samples, bed_fpath=bed_fpath, tag_by_sample=tag_by_sample)
+
+    # best_for_regions_fpath = None
+    # if any(verify_file(s.targqc_region_tsv, silent=True) for s in samples):
+    #     best_for_regions_fpath = _save_best_details_for_each_gene(tc.depth_thresholds, samples, output_dir)
+    # ''' 1. best_regions = get_best_regions()
+    #     2. best_for_regions_fpath = save_per_region_report()
+    #     3. calc median coverage across best regions
+    #     4. flagged_regions_report_fpath = _generate_flagged_regions_report(
+    #          output_dir, 'Best', average_coverage, genes, depth_threshs)
+    # '''
+
+    txt_region_rep_fpath, tsv_region_rep_fpath = _combined_regional_reports(work_dir, output_dir, samples)
+
+    info()
+    info('*' * 70)
+    if not html_fpath and not txt_fpath:
+        info('TargQC summary was not generated, because there were no reports generated for individual samples.')
+    else:
+        info('TargQC summary saved in: ')
+        info('  ' + html_fpath)
+        info('  ' + tsv_fpath + ' (parsable)')
+
+    info()
+    if not txt_region_rep_fpath and not tsv_region_rep_fpath:
+        info('Regional report was not generated, because there were no reports generated for individual samples.')
+    else:
+        info('Per-region coverage statistics saved into:')
+        info('  ' + txt_region_rep_fpath + ' (human-readable)')
+        info('  ' + tsv_region_rep_fpath + ' (parsable)')
+
+    return html_fpath
+
+
+def _prep_best_report(metric_storage, samples):
+    report = PerRegionSampleReport(sample='Best', metric_storage=metric_storage)
+
+    report.add_record('Sample', 'contains best values from all samples: ' + ', '.join([s.name for s in samples]))
+
+    m = metric_storage.find_metric('Average sample depth')
+    ave_sample_depth = max(BaseReport.find_record(s.report.records, m.name).value for s in samples)
+    report.add_record('Average sample depth', ave_sample_depth)
+
+    return report
+
+
+def _get_targqc_metric(metric, header_metric_storage, report_type='targetcov'):  # report type is in ['targetcov', 'qualimap', 'ngscat']
+    qualimap_to_targetcov_dict = {
+        'Number of reads': header_metric_storage.find_metric('Reads'),
+        'Mapped reads': header_metric_storage.find_metric('Mapped reads'),
+        'Unmapped reads': header_metric_storage.find_metric('Unmapped reads'),
+        'Mapped reads (on target)': header_metric_storage.find_metric('Reads mapped on target'),
+        'Coverage Mean': header_metric_storage.find_metric('Average target coverage depth'),
+        'Coverage Standard Deviation': header_metric_storage.find_metric('Std. dev. of target coverage depth')
+    }
+
+    ngscat_to_targetcov_dict = {
+        'Number reads': header_metric_storage.find_metric('Mapped reads'),
+        # '% target bases with coverage >= 1x': cov.header_metric_storage.get_metric('Percentage of target covered by at least 1 read'),
+        '% reads on target': header_metric_storage.find_metric('Reads mapped on target'),
+        'mean coverage': header_metric_storage.find_metric('Average target coverage depth')
+    }
+
+    if report_type == 'targetcov':
+        return metric
+    elif report_type == 'qualimap':
+        if metric.name in qualimap_to_targetcov_dict:
+            return qualimap_to_targetcov_dict[metric.name]
+        return metric
+    return metric
+
+
+def _get_targqc_metric_storage(metric_storages_by_report_type):
+    class SectionId:
+        def __init__(self, name, title):
+            self.name = name
+            self.title = title
+
+        def __hash__(self):
+            #return hash((self.name, self.title))
+            return hash(self.name)  # use title from the first metric_storage
+
+        def __eq__(self, other):
+            #return (self.name, self.title) == (other.name, other.title)
+            return self.name == other.name  # use title from the first metric_storage
+
+    metrics_by_sections = OrderedDict()
+    general_section_id = None
+    general_section_metric_list = []
+
+    for report_type, metric_storage in metric_storages_by_report_type:
+        for section in metric_storage.sections:
+            section_id = SectionId(section.name, section.title)
+            if section_id not in metrics_by_sections.keys():
+                metrics_by_sections[section_id] = []
+
+            metrics_by_sections[section_id] += [metric
+                for metric in metric_storage.get_metrics(sections=[section], skip_general_section=True)
+                if metric == _get_targqc_metric(metric, dict(metric_storages_by_report_type)['targetcov'], report_type)]
+
+        # specific behaviour for general section
+        general_section_metric_list += [metric
+            for metric in metric_storage.general_section.metrics
+            if metric == _get_targqc_metric(metric, dict(metric_storages_by_report_type)['targetcov'], report_type)]
+        if not general_section_id:
+            general_section_id = SectionId(metric_storage.general_section.name, metric_storage.general_section.title)
+
+    sections = []
+    for section_id, metric_list in metrics_by_sections.items():
+        sections.append(ReportSection(section_id.name, section_id.title, metric_list))
+
+    return MetricStorage(
+        general_section=ReportSection(
+            general_section_id.name, general_section_id.title, general_section_metric_list),
+        sections=sections)
+
+
+def _get_targqc_records(records_by_report_type, header_storage):
+    targqc_records = []
+    filled_metric_names = []
+    for report_type, records in records_by_report_type:
+        for record in records:
+            new_metric = _get_targqc_metric(record.metric, header_storage, report_type)
+            if not new_metric or new_metric.name not in filled_metric_names:
+                filled_metric_names.append(record.metric.name)
+                record.metric = new_metric
+                targqc_records.append(record)
+    return targqc_records
+
+
+def _correct_qualimap_genome_results(samples):
+    """ fixing java.lang.Double.parseDouble error on entries like "6,082.49"
+    """
+    for s in samples:
+        if verify_file(s.qualimap_genome_results_fpath):
+            correction_is_needed = False
+            with open(s.qualimap_genome_results_fpath, 'r') as f:
+                content = f.readlines()
+                metrics_started = False
+                for line in content:
+                    if ">> Reference" in line:
+                        metrics_started = True
+                    if metrics_started:
+                        if line.find(',') != -1:
+                            correction_is_needed = True
+                            break
+            if correction_is_needed:
+                with open(s.qualimap_genome_results_fpath, 'w') as f:
+                    metrics_started = False
+                    for line in content:
+                        if ">> Reference" in line:
+                            metrics_started = True
+                        if metrics_started:
+                            if line.find(',') != -1:
+                                line = line.replace(',', '')
+                        f.write(line)
+
+
+def _correct_qualimap_insert_size_histogram(work_dir, samples):
+    """ replacing Qualimap insert size histogram with Picard one.
+    """
+    for s in samples:
+        qualimap1_dirname = dirname(s.qualimap_ins_size_hist_fpath).replace('raw_data_qualimapReport', 'raw_data')
+        qualimap2_dirname = dirname(s.qualimap_ins_size_hist_fpath)
+        if exists(qualimap1_dirname):
+            if not exists(qualimap2_dirname):
+                shutil.move(qualimap1_dirname, qualimap2_dirname)
+            else:
+                shutil.rmtree(qualimap1_dirname)
+        elif not exists(qualimap2_dirname):
+            continue  # no data from both Qualimap v.1 and Qualimap v.2
+
+        # if qualimap histogram exits and reuse_intermediate, skip
+        if verify_file(s.qualimap_ins_size_hist_fpath, silent=True) and tc.reuse_intermediate:
+            pass
+        else:
+            if verify_file(s.picard_ins_size_hist_txt_fpath):
+                with open(s.picard_ins_size_hist_txt_fpath, 'r') as picard_f:
+                    one_line_to_stop = False
+                    for line in picard_f:
+                        if one_line_to_stop:
+                            break
+                        if line.startswith('## HISTOGRAM'):
+                            one_line_to_stop = True
+
+                    with file_transaction(work_dir, s.qualimap_ins_size_hist_fpath) as tx:
+                        with open(tx, 'w') as qualimap_f:
+                            for line in picard_f:
+                                qualimap_f.write(line)
+
+
+def select_best(values, fn=max):
+    vs = [v for v in values if v is not None]
+    return fn(vs) if len(vs) > 0 else None
+
+
+def get_int_val(v):
+    v = _get_num(v)
+    return int(v) if v else None
+
+def get_float_val(v):
+    v = _get_num(v)
+    return float(v) if v else None
+
+def _get_num(v):
+    v = get_val(v)
+    return ''.join(c for c in v if c.isdigit() or c == '.') if v else None
+
+def get_val(v):
+    return v.strip() if v.strip() not in ['.', '-', ''] else None
+
+
+def _save_best_details_for_each_gene(depth_threshs, samples, output_dir):
+    metric_storage = get_detailed_metric_storage(depth_threshs)
+
+    report = PerRegionSampleReport(sample='Best', metric_storage=metric_storage)
+    report.add_record('Sample', 'contains best values from all samples: ' + ', '.join([s.name for s in samples]))
+
+    total_regions = 0
+    fpaths = [s.targqc_region_tsv for s in samples if verify_file(s.targqc_region_tsv)]
+    if not fpaths:
+        err('No targetcov detailed per-gene report was generated; skipping.')
+        return None
+
+    open_tsv_files = [open(fpath) for fpath in fpaths]
+
+    first_col = 0
+    while True:
+        lines_for_each_sample = [next(f, None) for f in open_tsv_files]
+        if not all(lines_for_each_sample):
+            break
+        l = lines_for_each_sample[0]
+        if l.startswith('##'):
+            continue
+        elif l.startswith('#'):
+            if l.startswith('#Sample'):
+                first_col = 1
+            break
+
+    while True:
+        lines_for_each_sample = [next(f, None) for f in open_tsv_files]
+        if not all(lines_for_each_sample):
+            break
+
+        if all([not l.startswith('#') and ('Whole-Gene' in l or 'Gene-Exon' in l) for l in lines_for_each_sample]):
+            shared_fields = lines_for_each_sample[0].split('\t')[first_col:first_col+9]
+            reg = report.add_row()
+            reg.add_record('Chr', get_val(shared_fields[0]))
+            reg.add_record('Start', get_int_val(shared_fields[1]))
+            reg.add_record('End', get_int_val(shared_fields[2]))
+            reg.add_record('Size', get_int_val(shared_fields[3]))
+            reg.add_record('Gene', get_val(shared_fields[4]))
+            reg.add_record('Strand', get_val(shared_fields[5]))
+            reg.add_record('Feature', get_val(shared_fields[6]))
+            reg.add_record('Biotype', get_val(shared_fields[7]))
+            reg.add_record('Transcript', get_val(shared_fields[8]))
+
+            min_depths, ave_depths, stddevs, withins = ([], [], [], [])
+            percents_by_threshs = {t: [] for t in depth_threshs}
+
+            for l in lines_for_each_sample:
+                fs = l.split('\t')
+
+                min_depths.append(get_int_val(fs[first_col+9]))
+                ave_depths.append(get_float_val(fs[first_col+10]))
+                stddevs.append(get_float_val(fs[first_col+11]))
+                withins.append(get_float_val(fs[first_col+12]))
+                for t, f in zip(depth_threshs, fs[first_col+13:]):
+                    percents_by_threshs[t].append(get_float_val(f))
+
+            # counting bests
+            reg.add_record('Min depth', select_best(min_depths))
+            reg.add_record('Ave depth', select_best(ave_depths))
+            reg.add_record('Std dev', select_best(stddevs, max))
+            reg.add_record('W/n 20% of ave depth', select_best(withins))
+            for t in depth_threshs:
+                reg.add_record('{}x'.format(t), select_best(percents_by_threshs[t]))
+
+            total_regions += 1
+
+    for f in open_tsv_files:
+        f.close()
+
+    gene_report_basename = add_suffix(samples[0].targqc_region_tsv, 'best')
+    txt_rep_fpath = report.save_txt(join(output_dir, gene_report_basename + '.txt'))
+    tsv_rep_fpath = report.save_tsv(join(output_dir, gene_report_basename + '.tsv'))
+    info('')
+    info('Best values for the regions (total ' + str(total_regions) + ') saved into:')
+    info('  ' + txt_rep_fpath)
+
+    return txt_rep_fpath
+
+
+
+
