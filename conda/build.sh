12c203c3077da5223d516301b43ffea12b4b6f77
vladsaveliev <vladsaveliev@me.com>
2017-05-29 18:01:59 +0300

    Add readme script to build conda packages

---
+++ conda/build.sh
@@ -0,0 +1,14 @@
+conda build ngs_utils -c bcbio -c bioconda -c conda-forge --py 2.7
+conda build ngs_utils -c bcbio -c bioconda -c conda-forge --py 3.6
+cd /Users/vlad/miniconda3/conda-bld
+conda convert -p linux-32 osx-64/targqc-1.5.0-py36_2.tar.bz2
+conda convert -p linux-64 osx-64/targqc-1.5.0-py36_2.tar.bz2
+conda convert -p linux-32 osx-64/targqc-1.5.0-py27_2.tar.bz2
+conda convert -p linux-64 osx-64/targqc-1.5.0-py27_2.tar.bz2
+anaconda upload osx-64/targqc-1.5.0-py36_2.tar.bz2
+anaconda upload osx-64/targqc-1.5.0-py27_2.tar.bz2
+anaconda upload linux-32/targqc-1.5.0-py36_2.tar.bz2
+anaconda upload linux-32/targqc-1.5.0-py27_2.tar.bz2
+anaconda upload linux-64/targqc-1.5.0-py36_2.tar.bz2
+anaconda upload linux-64/targqc-1.5.0-py27_2.tar.bz2
+
