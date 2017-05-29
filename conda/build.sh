4ccab7d25cc1f8a33a3e0ecc29baded19f4f9320
vladsaveliev <vladsaveliev@me.com>
2017-05-29 18:08:42 +0300

    Conda readme update

--- conda/build.sh
+++ conda/build.sh
@@ -1,5 +1,5 @@
-conda build ngs_utils -c bcbio -c bioconda -c conda-forge --py 2.7
-conda build ngs_utils -c bcbio -c bioconda -c conda-forge --py 3.6
+conda build targqc -c vladsaveliev -c bcbio -c bioconda -c conda-forge --py 2.7
+conda build targqc -c vladsaveliev -c bcbio -c bioconda -c conda-forge --py 3.6
 cd /Users/vlad/miniconda3/conda-bld
 conda convert -p linux-32 osx-64/targqc-1.5.0-py36_2.tar.bz2
 conda convert -p linux-64 osx-64/targqc-1.5.0-py36_2.tar.bz2
