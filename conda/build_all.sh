6a87577c7bc8e533b23aa974e262429aa8ab6299
vladsaveliev <vladislav.sav@gmail.com>
2018-05-18 18:59:20 +1000

    Skip support py2

--- conda/build_all.sh
+++ conda/build_all.sh
@@ -19,38 +19,25 @@ function build() {
     echo "Building $NAME";
 
     CHANNELS="-c vladsaveliev -c bioconda -c defaults -c conda-forge"
-    for PY in 3.6 2.7 ; do
+    for PY in 3.6 ; do
         PACKAGE_PATH=$(conda build $NAME $CHANNELS --output --py $PY | tail -n1)
-        if [ -f $PACKAGE_PATH ] ; then
-            echo "$PACKAGE_PATH exists, skipping"
-        else
-            echo "Building $PACKAGE_PATH"
-            conda build $NAME $CHANNELS --py $PY
-            anaconda upload $PACKAGE_PATH
-            BASEDIR=$(dirname $(dirname $PACKAGE_PATH))
-            FILENAME=$(basename $PACKAGE_PATH)
-            echo "Converting packages into $BASEDIR"
-            for PLATFORM in osx-64 linux-32 linux-64 ; do
-                if [ -f $BASEDIR/$PLATFORM/$FILENAME ] ; then
-                    echo "$BASEDIR/$PLATFORM/$FILENAME exists, skipping"
-                else
-                    conda convert -p $PLATFORM $PACKAGE_PATH -o $BASEDIR -f
-                    anaconda upload $BASEDIR/$PLATFORM/$FILENAME
-                fi
-            done
-        fi
+        echo "Building $PACKAGE_PATH"
+        conda build $NAME $CHANNELS --py $PY
+        anaconda upload $PACKAGE_PATH
+        BASEDIR=$(dirname $(dirname $PACKAGE_PATH))
+        FILENAME=$(basename $PACKAGE_PATH)
     done
 }
 
-if [ -z "$1" ]; then
-    # Iterate over directories and build all packages
-    for f in */meta.yaml ; do
-        package_name=$(dirname ${f#/*});
-        build $package_name;
-    done
-else
-    build $1;  # Build specific package
-fi
+#if [ -z "$1" ]; then
+#    # Iterate over directories and build all packages
+#    for f in */meta.yaml ; do
+#        package_name=$(dirname ${f#/*});
+#        build $package_name;
+#    done
+#else
+build $1;  # Build specific package
+#fi
 
 set +x
 set +e
